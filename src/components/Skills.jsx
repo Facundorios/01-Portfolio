ff7a699d022cf7334da1ca6b2987fafd4b511beb
Facundo Ríos <Facundorios2005@gmail.com>
2024-09-14 20:12:00 -0300

    feat: :art: Se añadió estilo e información
    Se añadieron nuevos datos para el mapeo, se cambió la estructura para renderizar los arreglos, se mejoraron los diseños, espacios y se crearon hover.  se crea un archivo css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,45 +1,29 @@
 import React from "react";
 
-import { databases, languages, gitStats } from "../data/skills.data.js";
+import "../css/images.css";
+
+import {
+  databases,
+  languages,
+  gitStats,
+  frameworks,
+  tools,
+} from "../data/skills.data.js";
 
 export default function Skills() {
   return (
-    <div className="bg-slate-800 h-auto">
+    <div className="bg-slate-800 h-auto pl-5 pr-5">
       <h1 className="text-5xl font-mono text-center text-slate-300 shadow-slate-800">
         Mis habilidades
       </h1>
 
       <h2 className="text-2xl font-mono text-center text-slate-300">
-        Lenguajes de programación
-      </h2>
-
-      <div className="grid grid-cols-5 gap-10">
-        {languages.map((language) => {
-          return (
-            <div className="bg-slate-700 flex flex-col items-center">
-              <a href={language.documentation} target="_blank">
-                <img
-                  src={language.imageUrl}
-                  className="mt-5"
-                  width={75}
-                  alt={language.name}
-                />
-              </a>
-              <h1 className="text-sm text-gray-200 font-mono">
-                {language.name}
-              </h1>
-            </div>
-          );
-        })}
-      </div>
-
-      <h2 className="text-2xl font-mono text-center text-slate-300">
         Bases de datos
       </h2>
-      <div className="grid grid-cols-5 gap-5">
+      <div className="grid grid-cols-3 gap-3 mt-3 mb-3">
         {databases.map((database) => {
           return (
-            <div className="bg-slate-600 flex flex-col items-center">
+            <div className="card bg-slate-700 flex flex-col items-center card">
               <a href={database.documentation} target="_blank">
                 <img src={database.imageUrl} alt={database.name} width={75} />
               </a>
@@ -52,17 +36,75 @@ export default function Skills() {
       </div>
 
       <h2 className="text-2xl font-mono text-center text-slate-300">
+        Lenguajes de programación
+      </h2>
+
+      <div className="grid grid-cols-4 gap-5 mt-3 mb-3">
+        {languages.map((language) => {
+          return (
+            <div className="bg-slate-600 flex flex-col items-center card">
+              <a href={language.documentation} target="_blank">
+                <img
+                  src={language.imageUrl}
+                  className="mt-5"
+                  width={75}
+                  alt={language.name}
+                />
+              </a>
+              <h1 className="text-2xl text-gray-200 font-mono">
+                {language.name}
+              </h1>
+            </div>
+          );
+        })}
+      </div>
+      <h2 className="text-2xl font-mono text-center text-slate-300">
+        Framework/Biblotecas
+      </h2>
+
+      <div className="grid grid-cols-5 gap-5 mt-3 mb-3">
+        {frameworks.map((framework) => {
+          return (
+            <div className="bg-slate-500 flex flex-col items-center card">
+              <a href={framework.documentation} target="_blank">
+                <img src={framework.imageUrl} alt={framework.name} width={75} />
+              </a>
+              <h1 className="text-2xl text-gray-200 font-mono">
+                {framework.name}
+              </h1>
+            </div>
+          );
+        })}
+      </div>
+
+      <h2 className="text-2xl font-mono text-center text-slate-300">
+        Herramientas
+      </h2>
+      <div className="grid grid-cols-6 gap-5 mt-3 mb-3">
+        {tools.map((tool) => {
+          return (
+            <div className="bg-slate-400 flex flex-col items-center card">
+              <a href={tool.documentation} target="_blank">
+                <img src={tool.imageUrl} alt={tool.name} width={75} />
+              </a>
+              <h1 className="text-sm text-gray-200 font-mono">{tool.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+
+      <h2 className="text-2xl font-mono text-center text-slate-300">
         Estadísticas de GitHub
       </h2>
 
-      <div className="grid grid-cols-3 ">
+      <div className="grid grid-cols-3 mt-3 mb-3">
         {gitStats.map((gitStat) => {
           return (
             <div className="flex flex-col items-center ">
               <a href={gitStat.imageUrl} target="_blank">
                 <img
                   src={gitStat.imageUrl}
-                  className="shadow-xl dark:shadow-gray-300"
+                  className="card"
                   alt={gitStat.name}
                   width={400}
                   height={400}
--- src/data/skills.data.js
+++ src/data/skills.data.js
@@ -62,3 +62,94 @@ export const gitStats = [
       "https://github-readme-streak-stats.herokuapp.com/?user=facundorios",
   },
 ];
+
+export const frameworks = [
+  {
+    name: "React",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=123603&format=png&color=000000",
+    documentation: "https://reactjs.org/docs/getting-started.html",
+  },
+  {
+    name: "Bootstrap",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000",
+    documentation:
+      "https://getbootstrap.com/docs/5.0/getting-started/introduction/",
+  },
+  {
+    name: "Tailwind CSS",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000",
+    documentation: "https://tailwindcss.com/docs",
+  },
+  {
+    name: "Express",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=kg46nzoJrmTR&format=png&color=000000",
+    documentation: "https://expressjs.com/",
+  },
+
+  {
+    name: "NestJS",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=9ESZMOeUioJS&format=png&color=000000",
+    documentation: "https://docs.nestjs.com/",
+  },
+];
+
+export const tools = [
+  {
+    name: "Jira",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=oROcPah5ues6&format=png&color=000000",
+    documentation: "https://support.atlassian.com/jira-software-cloud/docs/",
+  },
+  {
+    name: "Trello",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=21049&format=png&color=000000",
+    documentation: "https://help.trello.com/",
+  },
+  {
+    name: "Docker",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=cdYUlRaag9G9&format=png&color=000000",
+    documentation: "https://docs.docker.com/",
+  },
+  {
+    name: "Git",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=20906&format=png&color=000000",
+    documentation: "https://git-scm.com/doc",
+  },
+  {
+    name: "Github",
+    imageUrl: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    documentation: "https://docs.github.com/en",
+  },
+  {
+    name: "Postman",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=EPbEfEa7o8CB&format=png&color=000000",
+    documentation:
+      "https://learning.postman.com/docs/getting-started/introduction/",
+  },
+  {
+    name: "VS Code",
+    imageUrl: "https://chris-ayers.com/assets/images/vscode-logo.png",
+    documentation: "https://code.visualstudio.com/docs",
+  },
+
+  {
+    name: "TablePlus",
+    imageUrl: "https://avatars.githubusercontent.com/u/29408238?s=280&v=4",
+    documentation: "https://tableplus.com/documentation",
+  },
+  {
+    name: "ViteJs",
+    imageUrl:
+      "https://img.icons8.com/?size=100&id=YO3YqSaTOu5K&format=png&color=000000",
+    documentation: "https://vitejs.dev/guide/",
+  },
+];
